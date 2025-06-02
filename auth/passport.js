4a9e6a980424fca2d3467d60c8add9ef3df8880c
pranay0217 <shitpranay@gmail.com>
2025-06-02 16:05:48 +0530

    Update passport.js

--- auth/passport.js
+++ auth/passport.js
@@ -9,25 +9,37 @@ passport.use(new GoogleStrategy({
   callbackURL: process.env.GOOGLE_CALLBACK_URL,
 }, async (accessToken, refreshToken, profile, done) => {
   try {
-    const email = profile.emails[0].value;
+    // Always prefer googleId as unique identifier
+    let user = await User.findOne({ googleId: profile.id });
 
-    let user = await User.findOne({ email });
+    if (!user) {
+      // If user not found by googleId, try to find by email (maybe signed up before)
+      const email = profile.emails[0].value;
+      user = await User.findOne({ email });
 
-    if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-      return done(null, { ...user.toObject(), token });
+      if (user) {
+        // If user exists with same email but no googleId, link googleId to existing user
+        user.googleId = profile.id;
+        await user.save();
+      }
     }
 
-    const newUser = new User({
-      username: profile.displayName,
-      email,
-      password: Math.random().toString(36).slice(-8), // dummy password
-    });
+    if (!user) {
+      // Create new user if none found by googleId or email
+      user = new User({
+        username: profile.displayName,
+        email: profile.emails[0].value,
+        googleId: profile.id,  // <-- Save googleId here!
+        password: Math.random().toString(36).slice(-8), // dummy password for now
+      });
+      await user.save();
+    }
 
-    await newUser.save();
+    // Create JWT token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-    return done(null, { ...newUser.toObject(), token });
+    // Return user object plus token
+    return done(null, { ...user.toObject(), token });
 
   } catch (error) {
     return done(error, null);
